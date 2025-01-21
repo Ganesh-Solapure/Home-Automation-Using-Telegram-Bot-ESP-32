3ba4955e2457bd87db2f8649421ea4dbfd5d99c7
GANESH SOLAPURE <105108194+Ganesh-Solapure@users.noreply.github.com>
2025-01-21 14:14:23 +0530

    Update allShutdown.ino

--- allShutdown.ino
+++ allShutdown.ino
@@ -13,6 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+
+https://github.com/Ganesh-Solapure/Home-Automation-Using-Telegram-Bot-ESP-32/blob/main/LICENSE
  */
 
 
